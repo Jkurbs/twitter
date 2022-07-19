abb47f530f8ed5bf02d731828b5e19ee59be61c9
Jkurbs <kerby.jean@hotmail.fr>
2022-07-19 11:02:29 -0400

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Kerby Jean on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
