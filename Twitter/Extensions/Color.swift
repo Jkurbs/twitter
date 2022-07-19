d6b86e14912d276f0d92e55676502beffd6cc7cd
Jkurbs <kerby.jean@hotmail.fr>
2022-07-19 12:59:08 -0400

    Added color scheme

--- Twitter/Extensions/Color.swift
+++ Twitter/Extensions/Color.swift
@@ -5,4 +5,23 @@
 //  Created by Kerby Jean on 7/19/22.
 //
 
-import Foundation
+import SwiftUI
+
+// Color schemes reference
+/*
+ https://usbrandcolors.com/twitter-colors/
+*/
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+class ColorTheme {
+    let primary = Color("Primary")
+    let secondary = Color("Secondary")
+    let label = Color("Label")
+    let secondaryLabel = Color("SecondaryLabel")
+    let quaternaryLabel = Color("QuaternaryLabel")
+    let placeholderText = Color("placeholderText")
+}
+
