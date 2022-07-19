c190a814502c008d4ff397f21bc97080d73cac52
Jkurbs <kerby.jean@hotmail.fr>
2022-07-19 19:23:01 -0400

    Added publishers to AuthViewModel

--- Twitter/ViewModels/AuthViewModel.swift
+++ Twitter/ViewModels/AuthViewModel.swift
@@ -1,8 +1,33 @@
 //
-//  Authentication.swift
+//  AuthViewModel.swift
 //  Twitter
 //
 //  Created by Kerby Jean on 7/19/22.
 //
 
+import Combine
+import FirebaseAuth
 import Foundation
+
+class AuthViewModel: ObservableObject {
+    
+    var authService = AuthService()
+    
+    @Published var username: String = ""
+    @Published var emailAddress: String = ""
+    @Published var password: String = ""
+    @Published var user: User? = nil
+    
+
+    init() { }
+
+    func signIn() {
+        authService.signIn(email: "kerby.jean@hotmail.fr", password: "123456") { authResult, error in
+          
+        }
+    }
+    
+    func logOut() {
+        
+    }
+}
