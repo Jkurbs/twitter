348524f6cf791e5120ee400714eab8734dfeaad7
Jkurbs <kerby.jean@hotmail.fr>
2022-07-19 12:45:21 -0400

    Created User and Tweet Class/Struct

--- Twitter/Models/Tweet.swift
+++ Twitter/Models/Tweet.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Tweet: Identifiable {
+    
+    let id: String
+    let text: String
+    let userId: String
+    let profileImage: String
+    let timestamp: String
+    let likes: Int
+}
--- Twitter/Models/User.swift
+++ Twitter/Models/User.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class User {
+    
+    let id: String
+    let profileImage: String
+    let emailAddress: String
+    var tweets: [Tweet]? = []
+
+    init(id: String, profileImage: String, emailAddress: String, tweets: [Tweet]? = []) {
+        self.id = id
+        self.profileImage = profileImage
+        self.emailAddress = emailAddress
+        self.tweets = tweets
+    }
+}
